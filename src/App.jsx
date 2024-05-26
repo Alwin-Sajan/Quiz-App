1d981bb1e7f31de1413ffd71c3e06494a83a455d
Alwin-Sajan <alwinsajan225@gmail.com>
2024-05-26 14:03:05 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,58 @@
 import React from 'react'
 // import lodoimg from 'Quiz-App/src/assets/lofi.jpg'
-// import { cData } from '@/assets/data'
+import { cData } from './/assets/data'
 // import {TeamCard} from '@/assets'
 import './App.css'
 const App = () => {
-  const cData = [
-    {
-          question: "How many days do we have in a week?",
-          answer: "Seven",
-          question_type: "General Knowledge For Kids"
-        }
-      ];
-    
+  // const cData = [
+  //   {
+  //         question: "How many days do we have in a week?",
+  //         answer: "Seven",
+  //         question_type: "General Knowledge For Kids"
+  //       }
+  //     ];
+  
+  
+  let cdv = 0; 
+  let datq = cData[cdv];
+  // button.addEventListener("click", myFunction);
+  const shoot = () => {
+     cdv++;
+    alert(cdv);
+    let datq = cData[cdv];
+
+    render(<App/>);
+  }
+  // const root = ReactDOM.createRoot(document.getElementById('main'));
+  // root.render(<App />);
 
   return (
     <div>
       <div className="Main">
         <h1>QUIZ APP</h1>
         <div className="qs">
-          <h2>Question : {cData[0].question} </h2>
+
+          <h2>
+            Question : {datq.question} 
+          </h2>
+
           <div className='ans'>
-          
-            
-          <h3>{ cData[0].answer}</h3>
-          
-          </div>  
+
+
+            <h3>
+              {datq.answer}
+            </h3>
+
+          </div>
           <p className='qtyp'>
-            { cData[0].question_type }
+            {datq.question_type}
 
           </p>
-              
+
           {/* <img src="lodoimg" id="lofiimg" alt="lofiimages" /> */}
         </div>
-        <button>Next</button>
+        <button onClick={shoot}>Take the Shot!</button>
+        
       </div>
     </div>
   )
